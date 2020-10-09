35a67c18c44f6b01cc9685f3ada711eb13f25855
Yutaka-Kudo <yttttt0201@gmail.com>
2020-10-10 03:41:22 +0900

    12th

--- config/settings.py
+++ config/settings.py
@@ -10,13 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+# from pathlib import Path
 import os
 import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -79,21 +79,19 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': BASE_DIR / 'postgresql',
+        'NAME': 'scraping',
         'USER': 'user',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
     }
 }
-
 
 
 # Password validation
