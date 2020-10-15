845f072174ded396ee4bbda3af89cf42eb83a5df
Yutaka-Kudo <yttttt0201@gmail.com>
2020-10-15 16:51:08 +0900

    scsh test

--- grg/grg_hp.py
+++ grg/grg_hp.py
@@ -17,6 +17,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.urls import reverse
+import os
 
 
 def grg_hp_sp(request):
@@ -63,155 +64,17 @@ def grg_hp_sp(request):
     # In[8]:
 
     sleep(1)
-
-    # In[9]:
-
-    user_name = "C329569"
-    pw = "fes130!!"
-
-    # In[10]:
-
-    # フォーム取得
-    id_input = driver.find_element_by_xpath("/html/body/div[2]/div/form/div/div[2]/table/tbody/tr[1]/td/input")
-    pw_input = driver.find_element_by_name('password')
-
-    # In[11]:
-
-    # 中身をクリア
-    id_input.clear()
-    pw_input.clear()
-
-    sleep(1)
-
-    # In[12]:
-
-    try:
-        # 入力
-        # driver.find_element_by_xpath(
-        #     "/html/body/main/div[2]/div/div/div[2]/dl/dd/form/div[3]/div[1]/label").click()
-        id_input.send_keys(user_name)
-        pw_input.send_keys(pw)
-    except Exception:
-        error_flg = True
-        print('インプットエラー')
-
-    # In[13]:
-
-    if error_flg is False:
-        try:
-            pw_input.submit()
-            sleep(2)
-        except Exception:
-            error_flg = True
-            print('ログインエラー')
-
-    # In[14]:
-
-    #店舗選択
-    if error_flg == False:
-        try:
-            elem = driver.find_element_by_link_text('Garage Kitchenあそび　西船橋店')
-            elem.click()
-            sleep(2)
-        except Exception:
-            error_flg = True
-            print('店舗選択エラー')
-    # In[15]:
-
-    #レポートボタンクリック
-    if error_flg == False:
-        try:
-            report_btn = driver.find_element_by_link_text('レポート')
-            report_btn.click()
-            sleep(2)
-        except Exception:
-            error_flg = True
-            print('レポートボタンクリックエラー')
-
-    # 操作ウィンドウを変更する
-    handle_array = driver.window_handles
-    driver.switch_to.window(handle_array[1])
-    import os
-    # In[16]:
     if __debug__:
-        download_dir = "/static/temp/"
+        download_dir = "static/tem/"
         os.mkdir(download_dir)
         driver.save_screenshot(download_dir + 'result.png')
     else:
-        download_dir = "/static/tem/"
+        download_dir = "static/temp/"
         os.mkdir(download_dir)
         driver.save_screenshot(download_dir + 'result.png')
-
+    # In[9]:
     
-    # In[17]:
-
-    # 本番用
-    # if error_flg is False:
-    try:
-        df_lists = []
-        i = 24  # 20
-        while i <= 25:
-            # 月選択
-            month_select_elem = driver.find_element_by_name('numberCd')
-            month_select_object = Select(month_select_elem)
-            month_select_object.select_by_index(i)
-            sleep(2)
-
-            # ここにデータ取得コードを。
-            df_list = pd.read_html(driver.page_source)
-            df_lists.append(df_list[4])
-
-            i += 1
-
-    except Exception:
-        error_flg = True
-        print('データ収集エラー')
-
-    # In[18]:
     driver.quit()
-
-    now = dt.datetime.now().strftime('%Y%m%d')
-
-    # In[19]:
-
-    # df_list_fix = []
-    # for df in df_lists:
-    #     df.columns = ['日にち', "天気", "合計", '店舗トップ', 'メニュー',
-    #                   '席・個室・貸切', '写真', 'こだわり', '地図', 'クーポン', '予約', 'その他']
-    #     df.drop('天気', axis=1, inplace=True)
-        # df.drop(df.index[list(range(len(df)-3,len(df)))],inplace=True)
-        # どちらでも可
-        # df.drop(df.tail(3).index, inplace=True)
-        # df.set_index('日にち', inplace=True)
-        # df.index = df.index.str.rstrip('(月火水木金土日)')
-        # df.index = pd.to_datetime(df.index, format='%Y/%m/%d')
-        # df.insert(0, "曜日", df.index.strftime('%a'))
-        # df['曜日'].replace({
-        #     "Mon": "月",
-        #     "Tue": "火",
-        #     "Wed": "水",
-        #     "Thu": "木",
-        #     "Fri": "金",
-        #     "Sat": "土",
-        #     "Sun": "日"
-        # }, inplace=True)
-
-        # df_list_fix.append(df)
-
-    # In[20]:
-
-    # df_fix = pd.concat([df_list_fix[i] for i in range(0, len(df_list_fix))])
-    df_fix = pd.concat([df_list[i] for i in range(0, len(df_list))])
-
-    # In[56]:
-
-    # oldpath = './data_garage_hp_sp_{}.csv'.format(now)
-
-    # response = HttpResponse(content_type='text/csv')
-    # response['Content-Disposition'] = 'attachment; filename={}'.format(oldpath)
-    # df_fix.to_csv(path_or_buf=response, sep=';', float_format='%.2f', index=False, decimal=",")
-    # return response
-    # return render(request, "scr/index.html")
     return redirect("/")
 
     # df_fix.to_csv(oldpath, mode="x", encoding="utf_8_sig")
