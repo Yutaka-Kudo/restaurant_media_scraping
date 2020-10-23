2bd019dc1dfffd1df64def4020f957edfaca5cb3
Yutaka-Kudo <yttttt0201@gmail.com>
2020-10-23 17:32:15 +0900

    delete nonusefile5

---
+++ collected_static/js/datepicker.js
@@ -0,0 +1,15 @@
+$('#datepicker').datepicker({
+    dateFormat: 'yy-mm',
+    monthNames: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
+    minViewMode: 'months'
+});
+$('#datepicker2').datepicker({
+    dateFormat: 'yy-mm',
+    monthNames: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
+    minViewMode: 'months'
+});
+$('#datepicker3').datepicker({
+    dateFormat: 'yy-mm',
+    monthNames: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
+    minViewMode: 'months'
+});
